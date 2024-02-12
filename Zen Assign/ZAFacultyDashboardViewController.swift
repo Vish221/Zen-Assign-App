d454bdf306413061e6a90e53297688eb01ad832f
Vishvesh Pratap Singh <vishveshpratapsingh31@gmail.com>
2024-02-12 10:46:08 +0530

    Initial Commit

--- Zen Assign/ZAFacultyDashboardViewController.swift
+++ Zen Assign/ZAFacultyDashboardViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ZADashboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .yellow
     }
 
 
---
+++ Zen Assign/ZAStudentDashboardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ZAStudentDashboardViewController.swift
+//  Zen Assign
+//
+//  Created by Vishvesh on 12/02/24.
+//
+
+import UIKit
+
+class ZAStudentDashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
